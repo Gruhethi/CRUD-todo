f89f860d8e5bb7aac59d2621bb7d199152d5995e
Gruhethi <130999236+Gruhethi@users.noreply.github.com>
2024-04-14 12:17:57 +0530

    config.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 
 export const  firebaseConfig = {
-        apiKey: "AIzaSyCdaha-oxmc1AKmRuIAM10W3NT3ig5lkBQ",
-        authDomain: "dev-assisto.firebaseapp.com",
-        projectId: "dev-assisto",
-        storageBucket: "dev-assisto.appspot.com",
-        messagingSenderId: "955105553521",
-        appId: "1:955105553521:web:1882735535ce41aa5fbfd0",
-        measurementId: "G-5TL89DXGD7",
+        apiKey: "",
+        authDomain: "",
+        projectId: "",
+        storageBucket: "",
+        messagingSenderId: "",
+        appId: "",
+        measurementId: "",
 };
